7416fc658fd27e9606ab8f30ea1b2b1c764f105f
Manon <manon.cvre@gmail.com>
2024-10-22 12:18:02 +0100

    fix: getAllReviews et getReviewById corrigées en objet

--- services/ReviewService.ts
+++ services/ReviewService.ts
@@ -1,23 +1,33 @@
 import { PrismaClient } from "@prisma/client";
 import { Review } from "../models/Review.js";
+import { UserService } from "./UserService.js";
+import { ProductService } from "./ProductService.js";
 
 const prisma = new PrismaClient();
-
+const userService = new UserService();
+const productService = new ProductService();
 export class ReviewService {
     //Ici, il faudrait se metre d'accord : est-ce qu'on stocke le userId et productId ou un User et un Product ? 
     //(le constructeur prend des objets alors que l'objet dans prisma a des Id)
     //jpense il faudrait mettre les Id, c'est mieux
 
     //D'ailleurs, c'est pas dans l'api mais est-ce qu'on peut récup les reviews by id et toutes les reviews?
-    
+
     async getAllReviews(): Promise<Review[]> {
         const reviews = await prisma.reviews.findMany();
-        const reviewList = reviews.map(
-            (review) => new Review(review.id, review.comment, review.grade, review.userId, review.productId)
+        const reviewList = await Promise.all(
+            reviews.map(
+                async (review) => {
+                    const user = await userService.getUserById(review.userId);
+                    const product = await productService.getProductById(review.productId);
+
+                    return new Review(review.id, review.comment, review.grade, user, product);
+                }
+            )
         );
         return reviewList;
     };
-    
+
     async getReviewById(reviewId: number): Promise<Review> {
         const review = await prisma.reviews.findUnique({
             where: {
@@ -28,13 +38,16 @@ export class ReviewService {
             throw new Error(`Commentaire avec l'ID ${reviewId} non trouvé.`);
         }
 
-        return new Review(review.id, review.comment, review.grade, review.userId, review.productId)
+        const user = await userService.getUserById(review.userId);
+        const product = await productService.getProductById(review.productId);
+
+        return new Review(review.id, review.comment, review.grade, user, product)
     };
 
-    async getReviewsByProduct(productId : number): Promise<Review[]> {
+    async getReviewsByProduct(productId: number): Promise<Review[]> {
         const reviews = await prisma.reviews.findMany({
-            where:{
-                productId:productId,
+            where: {
+                productId: productId,
             }
         });
         const reviewList = reviews.map(
@@ -43,10 +56,10 @@ export class ReviewService {
         return reviewList;
     };
 
-    async getReviewsByUser(userId : number): Promise<Review[]> {
+    async getReviewsByUser(userId: number): Promise<Review[]> {
         const reviews = await prisma.reviews.findMany({
-            where:{
-                userId:userId,
+            where: {
+                userId: userId,
             }
         });
         const reviewList = reviews.map(
@@ -55,23 +68,23 @@ export class ReviewService {
         return reviewList;
     };
 
-    async addReview(reviewComment : string | null, reviewGrade : number|null, userId : number, productId : number): Promise<Review> {
+    async addReview(reviewComment: string | null, reviewGrade: number | null, userId: number, productId: number): Promise<Review> {
         const existingReview = await prisma.reviews.findMany({
             where: {
-                productId : productId,
-                userId : userId,
+                productId: productId,
+                userId: userId,
             }
         });
         if (existingReview) {
             throw new Error(`L'utilisateur avec l'ID ${userId} a déjà écrit un commentaire pour le produit ${productId}`);
         };
-        
+
         const review = await prisma.reviews.create({
             data: {
-                comment : reviewComment,
+                comment: reviewComment,
                 grade: reviewGrade,
-                userId : userId,
-                productId : productId,
+                userId: userId,
+                productId: productId,
             }
         })
         return new Review(review.id, review.comment, review.grade, review.userId, review.productId);
