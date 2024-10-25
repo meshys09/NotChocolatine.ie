9ad3fbf847e79119065034f132963cd4e459a4ae
Manon <manon.cvre@gmail.com>
2024-10-25 15:25:29 +0100

    fix: import fix

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import userController from './controllers/UserController.js';
+import reviewController from './controllers/ReviewController.js';
+import productController from './controllers/ProductController.js';
+import orderController from './controllers/OrderController.js';
 
 const app = new Hono();
 
