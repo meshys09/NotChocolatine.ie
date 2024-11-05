ce413b485fab4985a7b4d7b1cd5d1483c53b6f3b
Lucie Trocmé <ltrocme35@gmail.com>
2024-11-05 16:17:28 +0000

    feat : add and display product elements

--- font-end/src/App.tsx
+++ font-end/src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import ProductTile from './ProductTile';
+import logo from './logoCroissant.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className='row'>
+        <div className='Left-column'>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </div>
+        <div className='Right-column'>
+        <p> NotChocolatine - Selling yummy pains au chocolats</p>
+        </div>
+        </div>
       </header>
+      <div className='Todays-products'>
+        <ProductTile name={"pain au chocolat"}/>
+        <ProductTile name={"croissant"}/>
+      </div>
     </div>
   );
 }
