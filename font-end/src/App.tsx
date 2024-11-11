31c1eb77fe2ae0b4fbb95d2eb9e978e58bec0b8f
Lucie Trocmé <ltrocme35@gmail.com>
2024-11-11 13:58:57 +0000

    style : styling the header

--- font-end/src/App.tsx
+++ font-end/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import ProductList from './Products/ProductList/ProductList';
 import './App.css';
+import CroissantLogo from './logoCroissant.png';
 import ProductPage from './Products/ProductPage/ProductPage';
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       <div className="App">
         {/* Barre de navigation */}
         <nav className="navbar">
-          <Link to="/" className="nav-link">Browse</Link>
+          <Link to="/" className="nav-link"> <img src={CroissantLogo} alt="logo" /> </Link>
           <Link to="/about" className="nav-link">Who Are We?</Link>
           <Link to="/cart" className="nav-link">Cart</Link>
           <Link to="/login" className="nav-link">Login</Link>
