1ad3d40f0eaf5a6bb1c6b18cc50b752f134f0587
Manon <manon.cvre@gmail.com>
2024-11-14 18:51:27 +0000

    feat: add routes for authentication

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ import userController from './controllers/UserController.js';
 import reviewController from './controllers/ReviewController.js';
 import productController from './controllers/ProductController.js';
 import orderController from './controllers/OrderController.js';
+import authController from './controllers/AuthenticationController.js';
 import { cors } from 'hono/cors';
 
 const app = new Hono();
@@ -11,7 +12,7 @@ const app = new Hono();
 app.use(
     '*',
     cors({
-        origin: 'http://localhost:4000', // Autorise l'origine de ton front-end
+        origin: 'http://localhost:3001', // Autorise l'origine de ton front-end
         allowMethods: ['GET', 'POST', 'PUT', 'DELETE'], // Méthodes autorisées
         allowHeaders: ['Content-Type', 'Authorization'] // En-têtes autorisés
     })
@@ -25,6 +26,7 @@ app.route('/users', userController);
 app.route('/reviews', reviewController);
 app.route('/products', productController);
 app.route('/orders', orderController);
+app.route('/auth', authController);
 
 export default app;
 
